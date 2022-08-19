25d5f9f4b8546c6cef40d0750a6184b1973d653d
Olena Demchyk <lenademchyk@gmail.com>
2022-08-19 02:36:50 +0200

    finished hw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,84 @@
+// Завдання 1 - галерея зображень
+// Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні. Подивися демо відео роботи галереї.
+// Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js. Розбий його на декілька підзавдань:
+
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+// Реалізація делегування на div.gallery і отримання url великого зображення.
+// Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
+// Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
+// Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
+// Розмітка елемента галереї
+// Посилання на оригінальне зображення повинно зберігатися в data-атрибуті source на елементі <img>, і вказуватися в href посилання. Не додавай інші HTML теги або CSS класи, крім тих, що містяться в цьому шаблоні.
+
+// {/* <div class="gallery__item">
+//   <a class="gallery__link" href="large-image.jpg">
+//     <img
+//       class="gallery__image"
+//       src="small-image.jpg"
+//       data-source="large-image.jpg"
+//       alt="Image description"
+//     />
+//   </a>
+// </div> */}
+// Зверни увагу на те, що зображення обгорнуте посиланням, отже по кліку за замовчуванням користувач буде перенаправлений на іншу сторінку. Заборони цю поведінку за замовчуванням.
+// Закриття з клавіатури
+// УВАГА
+// Наступний функціонал не обов'язковий для здавання завдання, але буде хорошою додатковою практикою.
+
+// Додай закриття модального вікна після натискання клавіші Escape. Зроби так, щоб прослуховування клавіатури було тільки доти, доки відкрите модальне вікно. Бібліотекаи basicLightbox містить метод для програмного закриття модального вікна.
+// =====================================================
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallaryContainer = document.querySelector('.gallery');
+const imagesMurkup = createGalleryImage(galleryItems);
+gallaryContainer.insertAdjacentHTML('beforeend', imagesMurkup);
 console.log(galleryItems);
+gallaryContainer.addEventListener('click', onGalleryContainerClick);
+
+function createGalleryImage(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  <div class="gallery__item">
+  <a class="gallery__link" href="${original}" rel="noreferrel noopener nofollow">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+  `;
+    })
+    .join('');
+}
+
+function onGalleryContainerClick(evt) {
+  evt.preventDefault();
+
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${
+      evt.target.closest('img').dataset.source
+    }" width="800" height="600">`,
+    {
+      onShow: () => window.addEventListener('keydown', onEscapeCloseModal),
+      onClose: () => window.removeEventListener('keydown', onEscapeCloseModal),
+    }
+  );
+
+  console.log(evt.target);
+  instance.show();
+
+  function onEscapeCloseModal(evt) {
+    if (evt.code === 'Escape') {
+      instance.close();
+      return;
+    }
+  }
+}
